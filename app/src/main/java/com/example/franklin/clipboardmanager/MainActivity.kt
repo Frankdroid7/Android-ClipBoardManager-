664b9b567c1845a03b919150d2b82a0fc6fc17f7
Franklin <ibukunoladipo2015@gmail.com>
2019-04-06 20:39:20 +0100

    Added simple functionality

--- app/src/main/java/com/example/franklin/clipboardmanager/MainActivity.kt
+++ app/src/main/java/com/example/franklin/clipboardmanager/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.franklin.clipboardmanager
 import android.content.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import android.content.ClipboardManager.OnPrimaryClipChangedListener
-import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Build
 import android.support.annotation.RequiresApi
-import android.support.v4.content.ContextCompat.getSystemService
+import android.support.design.widget.Snackbar
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
-
-
+import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var snackBar: Snackbar
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +22,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-  startService(Intent(this,DemoService::class.java))
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
+        copy_button.setOnClickListener {
+            val editText1Text = editText1.text.toString()
+
+            val mClip = ClipData.newPlainText("text", editText1Text)
+            clipboardManager.primaryClip = mClip!!
+
+            snackBar = Snackbar.make(constraint_root_tag, "Text Copied", Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss") { snackBar.dismiss() }
+            snackBar.show()
+
+        }
+
+        paste_button.setOnClickListener {
+            val editText2 = findViewById<EditText>(R.id.editText2)
+
+            val clipBoardText = clipboardManager.primaryClip!!.getItemAt(0).text
+            editText2.setText(clipBoardText)
+
+            snackBar = Snackbar.make(constraint_root_tag, "Text Pasted", Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss") { snackBar.dismiss() }
+            snackBar.show()
+
+        }
+
+        change_text_button.setOnClickListener {
+
+            val changeText = findViewById<EditText>(R.id.change_text_editText)
+            clipboardManager.primaryClip = ClipData.newPlainText("text", changeText.text.toString())
+
+            snackBar = Snackbar.make(constraint_root_tag, "Copied text changed to '${changeText.text}'",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss") { snackBar.dismiss() }
+            snackBar.show()
+
+        }
+
+        check_description_button.setOnClickListener {
+
+            val clipboardTextDesc = clipboardManager.primaryClip?.description?.label.toString()
+            snackBar = Snackbar.make(constraint_root_tag, "Copied Text Description is '$clipboardTextDesc'",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss"){ snackBar.dismiss() }
+            snackBar.show()
+
+        }
 
     }
 
 }
 
 
-//Toast.makeText(this,clipboardManager.primaryClip.toString(),Toast.LENGTH_LONG).show()
+
